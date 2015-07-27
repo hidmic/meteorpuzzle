f425ccbb26c39346b340d23e3c0f5f72c15bb757
Michel <hid.michel@gmail.com>
2015-07-27 01:28:01 -0300

    New design. Check for better linking ways.

--- client/puzzle.js
+++ client/puzzle.js
@@ -1,53 +1,110 @@
-Meteor.startup(function(){
+var Puzzle = new MongoDB.Collection('puzzle');
 
-    Session.set('piece')
+Meteor.startup(function(){
+    
 })
 
-Template.puzzle.onCreated(function(){
-    this.subscribe("boundPieces");
-    this.subscribe("unboundPieces");
+//method stubs
+Meteor.methods({
+    take: function(pieceId) {
+	var piece = Puzzle.findOne(pieceId);
+	if (piece.ownedBy != undefined) {
+	    throw new Meteor.Error("piece-owned", "The piece belongs to someone else!");
+	}
+	Puzzle.update(pieceId, {$set: {ownerId: this.userId}});	
+    },
+    move: function(pieceId, deltaX, deltaY) {
+	var piece = Puzzle.findOne(pieceId);
+	if (piece.ownedBy != this.userId) {
+	    throw new Meteor.Error("piece-not-owned", "The piece doesn't belong to you!");
+	}	
+    },
+    give: function(pieceId) {
+	var piece = Puzzle.findOne(pieceId);
+	if (piece.ownedBy != this.userId) {
+	    throw new Meteor.Error("piece-not-owned", "The piece doesn't belong to you!");
+	}
+	
+	Puzzle.update(pieceId, {$set: {ownedBy: undefined}});	
+    }
 });
 
+//Puzzle
 Template.puzzle.events({
-    'mousedown .unbound-piece': function (e) {
-	e.stopPropagation();
-	Meteor.call('takePiece', this.id, function(error) {
+    'mousemove': function() {
+	var lastX;
+	var lastY;
+	
+	return function(event) {
+	    var pieceId = Session.get('piece');
+	    if (pieceId) {
+		Meteor.call('move', pieceId, event.clientX - lastX, event.clientY - lastY, function(error) {
+		    if (error) {
+			
+			return;
+		    }
+		    //do nothing
+		});
+	    }		
+	    lastX = event.clientX;
+	    lastY = event.clientY;
+	}
+    }()
+});
+
+Template.puzzle.helpers({
+    pieces: function(){
+	return Puzzle.find({parentId: {$eq: undefined}});
+    }
+});
+
+//Pieces
+Template.piece.events({
+    'mousedown .orphan': function(event) {
+	event.stopPropagation();
+	Meteor.call('take', event.currentTarget.id, function(error) {
 	    if (error) {		
 		
 		return;
 	    }
-	    Session.set('piece', {pieceId: this.id, pointerX: , pointerY: });	    
-	});    
+	    Session.set('piece', event.currentTarget.id);
+	});    	    
     },
-    'mouseup .unbound-piece': function(e) {		
-	if ()
-	Meteor.call('givePiece', this.id, function(error) {
-	    if (error) {
+    'mouseup .orphan': function(event) {
+	event.stopPropagation();
+	Meteor.call('give', event.currentTarget.id, function(error) {
+	    if (error) {		
 		
 		return;
 	    }
-	    
-	});
+	    Session.set('piece', undefined);	    
+	});    	    	
+    }
+});
+
+Template.pieces.helpers({    
+    childs: function(){
+	return Puzzle.find({parentId: {$eq: this._id}});
     },
-    
-    'mousemove': function(e) {
-	var piece = Session.get('piece');
-	if (piece != undefined) {
-	    
-	    Meteor.call('movePiece', function(error, result) {
-		if (error) {
-		
-		    return;
-		}
-		
-	    });	    
+    height: function(){
+	return geoYmax(this.location) - geoYmin(this.location);
+    },
+    width: function(){
+	return geoXmax(this.location) - geoXmin(this.location);
+    },
+    top: function(){
+	return geoYmin(this.location);
+    },
+    left: function(){
+	return geoXmin(this.location);
+    },
+    condition: function(){
+	if (this.parentId == null) {
+	    return 'orhpan';
 	}
+	return return 'adopted';
+    },
+    orphan: function(){
+	return this.parentId == null;
     }
 });
-
-Template.puzzle.helpers({
-    xformCoord: function(){
-	
-    }
-});
-
--- server/puzzle.js
+++ server/puzzle.js
@@ -1,53 +1,53 @@
-Puzzle = new MongoDB.Collection('puzzle');
-
-Meteor.publish("boundPieces", function() {   
-    return Puzzle.find({bounded: {$eq true}});
-});
-
-Meteor.publish("unboundPieces", function() {
-    this._session.socket.on("close", function(){
-	Puzzle.update({ownedBy: {$eq this.userId}}, {$set: {ownedBy: null}});
-    });
-    return Puzzle.find({bounded: {$eq false}});
-});
+var Puzzle = new MongoDB.Collection('puzzle');
 
 Meteor.methods({
-    takePiece: function(pieceId) {
+    take: function(pieceId) {
 	var piece = Puzzle.findOne(pieceId);
-	if (piece.ownedBy != undefined) {
+	if (piece.ownedBy) {
 	    throw new Meteor.Error("piece-owned", "The piece belongs to someone else!");
+	}
+	if (piece.parentId) {
+	    throw new Meteor.Error("piece-adopted", "The piece has been adopted by another!");
 	}
 	Puzzle.update(pieceId, {$set: {ownedBy: this.userId}});	
     },
-    movePiece: function(pieceId, deltaX, deltaY) {
+    move: function(pieceId, deltaX, deltaY) {
 	var piece = Puzzle.findOne(pieceId);
 	if (piece.ownedBy != this.userId) {
 	    throw new Meteor.Error("piece-not-owned", "The piece doesn't belong to you!");
 	}
-	
+	if (piece.parentId) {
+	    throw new Meteor.Error("piece-adopted", "The piece has been adopted by another!");
+	}
+	var newLocation = geotransform(piece.location, function(x, y) { return [x+deltaX, y+deltaY]; });
+	Puzzle.update(pieceId, {$set: {location: newLocation}})	
     },
-    givePiece: function(pieceId) {
+    give: function(pieceId) {
 	var piece = Puzzle.findOne(pieceId);
 	if (piece.ownedBy != this.userId) {
 	    throw new Meteor.Error("piece-not-owned", "The piece doesn't belong to you!");
-	}
-	
-	Puzzle.update(pieceId, {$set: {ownedBy: undefined}});	
-    },
-    resetPieces: function() {
-
+	}	
+	if (piece.parentId) {
+	    throw new Meteor.Error("piece-adopted", "The piece has been adopted by another!");
+	}	
+	piece.ownedBy = undefined;
+	var theselinks = piece.links.map(function(link){ return {type: link.type, location: geotransform(link.location, piece.location)}; })
+	// any overlapped pieces?
+        Puzzle.find({_id: {$ne: pieceId}, parentId: {$eq: undefined}, location: {$geoIntersects: piece.location}}).forEach(function(other) {
+	    // any overlapped links?
+	    var thoselinks = other.links.map(function(link){ return {type: link.type, location: geotransform(link.location, other.location)}; })
+	    var pair = matchOne(thoselinks, thislinks, function(a, b) { return a.type == b.type && geointersects(a.location, b.location); });
+	    if (pair) {
+		var delta = geodelta(pair[0], pair[1]);
+		piece.location = geotransform(piece.location, function (x, y) { return [x+delta[0], y+delta[1]]; });
+		
+		piece.parentId = other._id;
+	    }
+	});	
+	Puzzle.update(pieceId, piece);		
     }
 });
 
 Meteor.startup(function () {
-    var pieceLocations = EJSON.parse(Assets.getText('wall_e'));
-    for(piece in pieceLocations) {
-	Puzzle.insert({
-	    ownedBy: undefined,
-	    bounded: false,
-	    currLoc: [-1, -1, -1, -1],
-	    trueLoc: pieceLocations[piece],
-	    imageUrl: 'wall_e/'+piece,
-	});
-    }	
+    
 });
